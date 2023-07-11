61eb2c00df66665d81e1f74c56e7b1533cc44598
Stephen O <stephenconochie@gmail.com>
2023-07-11 15:08:04 -0400

    Changes

--- Services/database.py
+++ Services/database.py
@@ -1,16 +1,19 @@
 import firebase_admin
+from firebase_admin import db, credentials
+import json
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path('Assets/.env')
-
-load_dotenv(dotenv_path=dotenv_path)
-
-APIKEY = os.getenv('DATABASE_URL')
-
-
-cred_obj = firebase_admin.credentials.Certificate('....path to file')
-default_app = firebase_admin.initialize_app(cred_obj, {
-    'databaseURL': APIKEY
+# authenticate to firebase
+cred_obj = credentials.Certificate('credentials.json')
+firebase_admin.initialize_app(cred_obj, {
+    "databaseURL": "https://stylemate-2f1e9-default-rtdb.firebaseio.com/"
 })
+
+# creating reference to root node
+ref = db.reference("/")
+
+# retrieving data from root node
+ref.get()
+
