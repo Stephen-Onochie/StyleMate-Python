b6bc3653dc03531a2b723244d46c6efdc863c14d
Stephen O <stephenconochie@gmail.com>
2023-07-11 07:59:27 -0400

    Added Some Firebase Code + Updated README

---
+++ Services/database.py
@@ -0,0 +1,16 @@
+import firebase_admin
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+dotenv_path = Path('Assets/.env')
+
+load_dotenv(dotenv_path=dotenv_path)
+
+APIKEY = os.getenv('DATABASE_URL')
+
+
+cred_obj = firebase_admin.credentials.Certificate('....path to file')
+default_app = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL': 'https://stylemate-2f1e9-default-rtdb.firebaseio.com/'
+})
