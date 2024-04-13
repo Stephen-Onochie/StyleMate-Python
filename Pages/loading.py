b84758042daee55e88b557d379454365387cf1bc
Stephen O <stephenconochie@gmail.com>
2024-04-13 19:17:46 -0400

    loading Screen

--- Pages/loading.py
+++ Pages/loading.py
@@ -7,8 +7,8 @@ root = tk.Tk()
 root.title("undefined")
 
 # setting window size
-width = 350
-height = 600
+width = 400
+height = 700
 screenwidth = root.winfo_screenwidth()
 screenheight = root.winfo_screenheight()
 alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
@@ -17,7 +17,7 @@ root.resizable(width=False, height=False)
 
 # Load and display an image
 # (replace 'your_logo.png' with the path to your image file)
-image = Image.open(r'F:\steph\Documents\Github\StyleMate-Python\Assets\Screen_Images\StyleMate_Loading_Background.png')
+image = Image.open(r'/Users/stepheno/Documents/GitHub/StyleMate-Python/Assets/Screen_Images/StyleMate_Loading_Background.png')
 image = ImageTk.PhotoImage(image)
 
 # Create a label to display the image
